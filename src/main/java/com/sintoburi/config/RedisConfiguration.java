8de63a49843ac8bc2ad15fcb5a2dc05f3a18c52b
JUN <chaeyjs827@gmail.com>
2021-09-10 23:25:07 +0900

    2021.09.10 commit

--- src/main/java/com/sintoburi/config/RedisConfiguration.java
+++ src/main/java/com/sintoburi/config/RedisConfiguration.java
@@ -1,34 +1,27 @@
 package com.sintoburi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfiguration {
 
 	private final String redisHost = "localhost";
 	private final int redisPort = 6379;
-//
-//	@Bean
-//	public RedisConnectionFactory connectionFactory() {
-//		return new LettuceConnectionFactory(redisHost, redisPort);
-//	}
-//
-//	@Bean
-//	public RedisTemplate<String, Object> redisTemplate() {
-//		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-//		redisTemplate.setKeySerializer(new StringRedisSerializer());
-//		redisTemplate.setValueSerializer(new StringRedisSerializer());
-//		redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-//		redisTemplate.setHashValueSerializer(new StringRedisSerializer());
-//		redisTemplate.setConnectionFactory(connectionFactory());
-//		return redisTemplate;
-//	}
-//	
-//	@Bean
-//	public StringRedisTemplate strRedisTemplate() {
-//		StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
-//		stringRedisTemplate.setConnectionFactory(connectionFactory());
-//		return stringRedisTemplate;
-//	}
 
+	@Bean
+	public RedisConnectionFactory redisConnectionFactory() {
+		return new LettuceConnectionFactory(redisHost, redisPort);
+	}
+	
+	@Bean
+	public RedisTemplate<?, ?> redisTemplate() {
+		RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(redisConnectionFactory());
+		return redisTemplate;
+	}
+	
 }
