1e58a34fb8058f1a5af2dafb9a514bdf4a01c6b5
seongnamfc <chaeyjs827@gmail.com>
2021-10-04 21:19:49 +0900

    2021.10.04 commit

--- src/test/java/com/sintoburi/test/RedisTest.java
+++ src/test/java/com/sintoburi/test/RedisTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,6 +20,7 @@ public class RedisTest {
 	private StringRedisTemplate redisTemplate;
 	
 	@Test
+	@Cacheable(cacheNames = "test", value = "healthy", key ="#mask")
 	public void testString() throws Exception {
 	
 		final String key = "testJun";
