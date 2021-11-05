83db5a493d1514ae5b49b61844b6b77dca0bb965
seongnamfc <chaeyjs827@gmail.com>
2021-11-05 17:38:22 +0900

    2021.11.05 commit

---
+++ src/main/java/com/sintoburi/auth/AuthInterceptor.java
@@ -0,0 +1,12 @@
+package com.sintoburi.auth;
+
+/**
+ * @author seongnamfc
+ * @package com.sintoburi.auth
+ * @file AuthInterceptor
+ * @description
+ * @date 2021/11/05
+ */
+public class AuthInterceptor {
+
+}
