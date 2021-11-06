649bf0ae8177d4a90deca1de184209056f5cc86b
seongnamfc <chaeyjs827@gmail.com>
2021-11-06 09:42:20 +0900

    2021.11.06 commit

--- src/main/java/com/sintoburi/auth/AuthInterceptor.java
+++ src/main/java/com/sintoburi/auth/AuthInterceptor.java
@@ -1,5 +1,11 @@
 package com.sintoburi.auth;
 
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @author seongnamfc
  * @package com.sintoburi.auth
@@ -7,6 +13,13 @@ package com.sintoburi.auth;
  * @description
  * @date 2021/11/05
  */
-public class AuthInterceptor {
+public class AuthInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // jwt 토큰 검증 로직
+
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
 
 }
