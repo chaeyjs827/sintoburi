7effbccfc2022d7f995d54bb9fa7c16b1c00a1a1
seongnamfc <chaeyjs827@gmail.com>
2021-11-07 22:30:07 +0900

    2021.11.07 commit

--- src/main/java/com/sintoburi/auth/AuthInterceptor.java
+++ src/main/java/com/sintoburi/auth/AuthInterceptor.java
@@ -4,6 +4,7 @@ import com.google.common.net.HttpHeaders;
 import com.sintoburi.util.UtilJwt;
 import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -23,6 +24,23 @@ public class AuthInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        // HandlerMethod 타입 체크
+        if(handler instanceof HandlerMethod == false) {
+            return true;
+        }
+
+        // 형 변환
+        HandlerMethod handlerMethod = (HandlerMethod)handler;
+
+        // @AuthRequired 객체 불러오기
+        AuthRequired auth = handlerMethod.getMethodAnnotation(AuthRequired.class);
+
+        // method에 @AuthRequired 가 없는 경우
+        if(auth == null) {
+            return true;
+        }
+
         // jwt 토큰 검증 로직
         System.out.println("AuthInterceptor preHandle executed !!");
 
