8bdffed76683de04012cc0c0ff1cd26e7fdc406e
seongnamfc <chaeyjs827@gmail.com>
2021-11-19 23:26:48 +0900

    2021.11.19 commit - hmacSha256 메소드 에러 발생 하는중....

--- src/main/java/com/sintoburi/util/UtilJwt.java
+++ src/main/java/com/sintoburi/util/UtilJwt.java
@@ -192,22 +192,27 @@ public class UtilJwt extends JwtConfig {
 
 	}
 
+//	public String testToken(JwtDto jwtDto) {
+//		return hmacSha256(null, jwtDto.getPayload());
+//	}
+
 	private String hmacSha256(String data, String secret) {
-//		try {
-//
-//			byte[] hash = secret.getBytes(StandardCharsets.UTF_8);
-//			Mac sha256Hmac = Mac.getInstance("HmacSHA256");
-//			SecretKeySpec secretKey = new SecretKeySpec(hash, "HmacSHA256");
-//			sha256Hmac.init(secretKey);
-//
-//			byte[] signedBytes = sha256Hmac.doFinal(data.getBytes(StandardCharsets.UTF_8));
-//
-//			return encode(signedBytes);
-//		} catch (NoSuchAlgorithmException | InvalidKeyException ex) {
-//			Logger.getLogger(JWebToken.class.getName()).log(Level.SEVERE, ex.getMessage(), ex);
-//			return null;
-//		}
-		return null;
+		try {
+			byte[] hash = secret.getBytes(StandardCharsets.UTF_8);
+			Mac sha256Hmac = Mac.getInstance("HmacSHA256");
+			SecretKeySpec secretKey = new SecretKeySpec(hash, "HmacSHA256");
+			sha256Hmac.init(secretKey);
+
+			byte[] signedBytes = sha256Hmac.doFinal(data.getBytes(StandardCharsets.UTF_8));
+
+			return encode(signedBytes);
+		} catch (NoSuchAlgorithmException | InvalidKeyException ex) {
+			return null;
+		}
+	}
+
+	private String encode(byte[] bytes) {
+		return Base64.getUrlEncoder().withoutPadding().encodeToString(bytes);
 	}
 
 //	signature = hmacSha256(encodedHeader + "." + encode(payload));
