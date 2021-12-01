be899e230ea891fe2535a6322bf9c5ba9e256b5a
seongnamfc <chaeyjs827@gmail.com>
2021-12-01 22:48:01 +0900

    2021.12.01 commit

---
+++ src/main/java/com/sintoburi/service/StRedisServiceImpl.java
@@ -0,0 +1,18 @@
+package com.sintoburi.service;
+
+import com.sintoburi.service.impl.StRedisService;
+
+/**
+ * @author seongnamfc
+ * @package com.sintoburi.service
+ * @file StRedisServiceImpl
+ * @description
+ * @date 2021/12/01
+ */
+public class StRedisServiceImpl implements StRedisService {
+
+    @Override
+    public void save(String cacheName, Object key, Object data) {
+
+    }
+}
