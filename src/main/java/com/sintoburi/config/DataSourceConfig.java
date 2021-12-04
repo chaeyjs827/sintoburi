e1036ef8119c771b849ce0823f73581376b3472f
seongnamfc <chaeyjs827@gmail.com>
2021-12-04 23:36:32 +0900

    2021.12.04 commit

---
+++ src/main/java/com/sintoburi/config/DataSourceConfig.java
@@ -0,0 +1,11 @@
+package com.sintoburi.config;
+
+/**
+ * @author seongnamfc
+ * @package com.sintoburi.config
+ * @file DataSourceConfig
+ * @description
+ * @date 2021/12/04
+ */
+public class DataSourceConfig {
+}
